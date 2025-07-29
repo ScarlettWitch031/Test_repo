b0f62dd69ef2afec3421cec99055408dbe946ae1
test <scarlettwitch031@gmail.com>
2025-07-29 18:24:54 +0530

    a js file to get test

--- test.js
+++ test.js
@@ -14,3 +14,10 @@ app.get('/vulnerable', (req, res) => {
  }
 });
 
+app.get('/vulnerable', (req, res) => {
+ if (req.query.url) {
+ 	res.redirect(req.query.url);
+ } else {
+ 	res.redirect('<https://www.example.com>');
+ }
+});
