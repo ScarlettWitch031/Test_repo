cdf28a7f2590b153fdbbb6098ff25f2a9acb5d24
test <scarlettwitch031@gmail.com>
2025-07-29 16:44:29 +0530

    qwe js file one more

--- test2.js
+++ test2.js
@@ -13,3 +13,11 @@ app.get('/vulnerable', (req, res) => {
  	res.redirect('<https://www.example.com>');
  }
 });
+
+app.get('/vulnerable', (req, res) => {
+ if (req.query.url) {
+ 	res.redirect(req.query.url);
+ } else {
+ 	res.redirect('<https://www.example.com>');
+ }
+});
